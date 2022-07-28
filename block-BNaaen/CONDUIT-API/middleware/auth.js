5a1dcded1eb71d383d8c166ea8f79f8841954fde
umakant-2113 <lodhiumakant800@gmail.com>
2022-07-28 22:52:35 +0530

    Done

---
+++ block-BNaaen/CONDUIT-API/middleware/auth.js
@@ -0,0 +1,18 @@
+let jwt = require('jsonwebtoken');
+
+module.exports = {
+  verifyToken: async (req, res, next) => {
+    let token = req.headers.authorization;
+    try {
+      if (token) {
+        let payload = await jwt.verify(token, process.env.secret);
+        req.users = payload;
+        next();
+      } else {
+        res.status(400).json({ Error: 'token is required' });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+};
