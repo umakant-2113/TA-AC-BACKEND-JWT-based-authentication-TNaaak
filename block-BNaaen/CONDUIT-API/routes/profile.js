0962ea2b0ff9761f9db76c6fb989113834378aa5
umakant-2113 <lodhiumakant800@gmail.com>
2022-07-30 09:20:00 +0530

    Done

--- block-BNaaen/CONDUIT-API/routes/profile.js
+++ block-BNaaen/CONDUIT-API/routes/profile.js
@@ -7,34 +7,74 @@ router.use(auth.verifyToken)
 
 router.get('/:username', async (req, res, next) => {
   let username = req.params.username;
-  let user = await User.find({ username });
-  res.status(200).json(user);
+  let usr = await User.find({ username });
+
+  let profile = {
+    name: usr.name,
+    username: usr.username,
+    email: usr.email,
+    bio: usr.bio,
+    following: usr.following
+}
+return res.json({ profile })
 });
 
 router.post('/:username/follow', async (req, res, next) => {
   let loginUserId =req.users.userId;
+  console.log(req.users)
   let username = req.params.username
-  let user = await User.find({ userName:username });
-let updateLoginUser= await User.findByIdAndUpdate(loginUserId,{$push:{followUser: user[0].id}},{new:true});
-if(updateLoginUser.followUser.includes(user[0].id)){
-  res.json({Follow : true})
-}else{
-  res.json({UnFollow:true})
-}
+  let user = await User.findOne({ userName:username });
+   let loginUser= await User.findById(loginUserId)
+   if(loginUser.followUser.includes(user[0].id)){
+    res.json({follow: "this user is already follow"})
+   }
+   if(!loginUser.followUser.includes(user[0].id)){
+   let loginUser= await User.findByIdAndUpdate(loginUserId, {$push : {followUser: user[0].id}},{new:true});
+   res.json({follow:true})
+   }
+
+ 
 });
 
 // unfollow
 
-router.delete("/:username/unfollow", async(req,res,next)=>{
-let loginUserId= req.users.userId;
-let username= req.params.username;
-let user= await User.find({userName: username});
-let loginuser= await User.findById(loginUserId);
-if( !loginuser.unfollowUser.includes(user[0].id)){
-res.json({UnFollow:true})
-}else{
-  res.json({Follow:true})
-}
+router.delete('/:username/follow', async (req, res, next) => {
+  let username = req.params.username
+  let loggedInUser = req.users.userId
+
+  try {
+      let unfollowedUser = await User.findOne({ username })
+
+      // //no followed user
+      if (!unfollowedUser) {
+          return res.status(400).json({ error: `${username} is not found` })
+      }
+      //checking loggedInUser and followed user are not same
+      if (unfollowedUser._id.equals(loggedInUser)) {
+          return res.status(400).json({ error: 'You Can`t unfollow Yourself' })
+      }
+
+      //checking if user is alredy followed or not
+      if (unfollowedUser.follow.includes(loggedInUser)) {
+          //finally unfollowed
+          let usr = await User.findOneAndUpdate({ username }, { $pull: { follow: loggedInUser } }, { new: true })
+          usr.following = false
+
+          let profile = {
+              name: usr.name,
+              username: usr.username,
+              email: usr.email,
+              bio: usr.bio,
+              following: usr.following
+          }
+          return res.json({ profile })
+      }
+
+      return res.json({ error: `You Have already unfollowed ${username}` })
+
+  } catch (error) {
+      next(error)
+  }
 })
 
 
